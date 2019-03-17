a627c4ad760adf80b3906e0b309982e17629d109
jrobind <jrobind@outlook.com>
2019-03-17 00:20:56 +0000

    add hangman game brief

---
+++ games/hangman.py
@@ -0,0 +1,8 @@
+# Hangman game using the CLI. A word is selected from a pre-made list, and the user should
+# attempt to guess the word with a limited number of attempts. Only one letter is to be
+# submitted by the user at a time. 
+
+class Hangman(object):
+
+    def __init__(self):
+        
