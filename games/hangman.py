b6305af9ebc266e3de6abb44cb1ecacf76f8c6d3
jrobind <jrobind@outlook.com>
2019-03-20 20:49:23 +0000

    add initial letter guessing logic

--- games/hangman.py
+++ games/hangman.py
@@ -3,6 +3,32 @@
 # submitted by the user at a time. 
 
 class Hangman(object):
-
     def __init__(self):
-        
+        # render inital game command
+        self.guesses_made = 0
+        self.correct_guess = False
+        self.letter_guesses = []
+        self.word_guesses = []
+        # run game
+        self.run_game()
+
+    def run_game(self):
+        while (not(self.correct_guess)):
+            self.get_guesses()
+
+    def get_guesses(self):
+        self.guesses_made += 1
+        self.letter_guesses.append(raw_input('Please guess the first letter of the word: '))
+        # run checks
+        self.run_checks()
+    
+    def run_checks(self):
+        if (self.guesses_made > 0):
+            self.word_guesses.append(raw_input(
+                'Current guessed letters: ' + self.format_letters(self.letter_guesses) + ' Please guess the word:  '))
+
+    def format_letters(self, letters):
+        return  ' '.join(letters)
+
+# initiate game
+Hangman()
