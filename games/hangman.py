c9fd8ec2b678959495f36a25f69ac5e1d00c8daa
jrobind <jrobind@outlook.com>
2019-03-30 10:59:04 +0000

    added additional user feedback & correct answer logic

--- games/hangman.py
+++ games/hangman.py
@@ -4,7 +4,8 @@
 
 class Hangman(object):
     def __init__(self):
-        # render inital game command
+        # temporary test word for now
+        self.word = 'test'
         self.guesses_made = 0
         self.correct_guess = False
         self.letter_guesses = []
@@ -26,9 +27,17 @@ class Hangman(object):
         if (self.guesses_made > 0):
             self.word_guesses.append(raw_input(
                 'Current guessed letters: ' + self.format_letters(self.letter_guesses) + ' Please guess the word:  '))
-
+            # loop over guesses to check if any are correct
+            for guess in self.word_guesses:
+                if (guess == self.word):
+                    self.correct_guess = True
+                    self.winMessage()
+            
     def format_letters(self, letters):
         return  ' '.join(letters)
+
+    def winMessage(self):
+        print('Well done! You guessed correct! The correct word is ' + self.word)
 
 # initiate game
 Hangman()
